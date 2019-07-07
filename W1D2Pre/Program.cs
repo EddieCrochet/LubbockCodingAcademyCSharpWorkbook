be184e1a62863b4683f435d4843e2de9f00e07e0
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-07-07 18:15:14 -0500

    Finished W1D2Pre

---
+++ W1D2Pre/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace W1D2Pre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Hello world! It is a beautiful day!! Yippee!!!";
+            string[] stringArray = sentence.Split('!');
+            foreach (string s in stringArray)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
