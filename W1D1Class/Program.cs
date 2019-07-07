87a41660550e6deaddac455ca4cd76a71e4cdfe0
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-07-07 17:53:13 -0500

    Finished W1D1Class

---
+++ W1D1Class/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace W1D1Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CatDog();
+        }
+
+        static void hello()
+        {
+            Console.WriteLine("Hello, User!");
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Bye {0}", userName);
+        }
+
+        static void CatDog()
+        {
+            Console.WriteLine("Do you prefer cats, or dogs????");
+            string userResponse = Console.ReadLine();
+
+            if (userResponse == "cats")
+            {
+                Console.WriteLine("MEOW");
+            }
+            else if (userResponse == "dogs")
+            {
+                Console.WriteLine("BARK");
+            }
+        }
+    }
+}
