3492e9ca5238ae4ffa51e0c789c15fa6d481fa7a
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-07-07 16:50:08 -0500

    original example thought through

---
+++ W1D1Pre/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace W1D1Pre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "100";
+            int myInt = 30;
+
+            int myResult = myInt + int.Parse(myString);
+            Console.WriteLine(myResult);
+        }
+    }
+}
