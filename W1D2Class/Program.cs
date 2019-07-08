c2e1b49714219a386fe7371c992ef8874f0d2b6d
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-07-07 20:45:55 -0500

    Finished W1D2Class

---
+++ W1D2Class/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace W1D2Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter an english word to convert to pig latin");
+            string english = Console.ReadLine();
+            string pigLatin = TranslateWord(english);
+            Console.WriteLine(pigLatin);
+        }
+
+        public static string TranslateWord(string word)
+        {
+            string[] vowels = { "a", "e", "i", "o", "u", "y" };
+            int firstLetterNum = Array.IndexOf(vowels, word.Substring(0, 1));
+            int lastLetterNum = Array.IndexOf(vowels, word.Substring(word.Length - 1, 1));
+            if(firstLetterNum>-1 && lastLetterNum>-1)
+            {
+                return word + "yay";
+            }
+            return word;
+        }
+    }
+}
